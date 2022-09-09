ed0572b8d8621b0c802126faca3bddf249e549e3
chandreshmage4 <chandresh@mage4.com>
2022-09-09 18:38:04 +0500

    add save data to db

--- app/code/Mage4/StoreLocator/Controller/Adminhtml/Item/Save.php
+++ app/code/Mage4/StoreLocator/Controller/Adminhtml/Item/Save.php
@@ -4,20 +4,27 @@ namespace Mage4\StoreLocator\Controller\Adminhtml\Item;
 
 use Mage4\StoreLocator\Model\ImageUploader;
 use Mage4\StoreLocator\Model\ItemFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Api\DataObjectHelper;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Model\AbstractModel;
 
-class Save extends \Magento\Backend\App\Action
+class Save extends Action
 {
     private $itemFactory;
     private $imageUploader;
 
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        ItemFactory $itemFactory,
-        ImageUploader $imageUploader
-    ) {
+        Context $context,
+        ItemFactory                         $itemFactory,
+        ImageUploader                       $imageUploader,
+        DataObjectHelper                    $dataObjectHelper
+    )
+    {
         $this->itemFactory = $itemFactory;
         $this->imageUploader = $imageUploader;
+        $this->dataObjectHelper = $dataObjectHelper;
         parent::__construct($context);
     }
 
@@ -26,24 +33,29 @@ class Save extends \Magento\Backend\App\Action
      */
     public function execute()
     {
-        $data= $this->getRequest()->getPostValue();
-//        $this->itemFactory->create()->setData($this->getRequest()->getParams())->save();
+        $data = $this->getRequest()->getParams();
+
+        $model = $this->itemFactory->create();
+        $model->load($data['id']);
+
+        if (!$model->getId()) {
+            unset($data['id']);
+        }
 
 
         if ($data) {
             if (isset($data['image'][0]['name']) && isset($data['image'][0]['tmp_name'])) {
-                $data['image'] =$data['image'][0]['name'];
-                /** @var ImageUploader $this->imageUploader */
+                $data['image'] = $data['image'][0]['name'];
+                /** @var ImageUploader $this- >imageUploader */
                 $this->imageUploader->moveFileFromTmp($data['image']);
+                $this->dataObjectHelper->populateWithArray($model, $data, AbstractModel::class);
             } elseif (isset($data['image'][0]['image']) && !isset($data['image'][0]['tmp_name'])) {
                 $data['image'] = $data['image'][0]['image'];
             } else {
                 $data['image'] = null;
             }
+            $model->setData($data)->save();
         }
-//        dd($data);
-
-        $this->itemFactory->create()->setData($this->getRequest()->getParams())->save();
         return $this->resultRedirectFactory->create()->setPath('storelocator/index/index');
     }
 }
