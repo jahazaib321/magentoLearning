a814152848ea8a911da87f5ea08fd1746e057982
chandreshmage4 <chandresh@mage4.com>
2022-09-20 23:59:34 +0500

    code cleanup

---
+++ app/code/Mage4/StoreLocator/Model/Resolver/StoresResolver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mage4\StoreLocator\Model\Resolver;
+
+use Mage4\StoreLocator\Model\ResourceModel\Store\CollectionFactory;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class StoresResolver implements ResolverInterface
+{
+    private $collectionFactory;
+
+    public function __construct(
+        CollectionFactory $collectionFactory
+    ) {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $collection = $this->collectionFactory->create();
+        return [
+            'total_count' => $collection->count(),
+            'items' => [
+                ['name' => "test", 'address' => "sadas"],
+                ['name' => "asdas", 'address' => "sadas"],
+                ['name' => "aasasd", 'address' => "sss"],
+                ['name' => "dasd", 'address' => "sadas"],
+            ]
+        ];
+    }
+}
