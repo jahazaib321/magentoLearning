0b9a289b98de037f987bd02b7af6e9b9c0a54da5
jahazaib321 <jahazaib@mage4.com>
2022-09-21 19:59:37 +0500

    add new folder to db

---
+++ app/code/Mage4/StoreLocator/Model/ResourceModel/Store/Grid/Collection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mage4\StoreLocator\Model\ResourceModel\Store\Grid;
+
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Psr\Log\LoggerInterface as Logger;
+
+class Collection extends \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult
+{
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+                      $mainTable = 'mage4_storelocator_managestores',
+                      $resourceModel = 'Mage4\StoreLocator\Model\ResourceModel\Store'
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $mainTable,
+            $resourceModel
+        );
+    }
+}
+
